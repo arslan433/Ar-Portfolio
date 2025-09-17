d5c403b5cf5337483ba46cf7a6229b78eecfd0d3
arslan433 <arslan.reactdev@gmail.com>
2025-09-17 14:19:25 +0500

    first Push

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+
+import ThemeToggle from "./ThemeToggle";
+export default function Navbar() {
+    return (
+        <div className="items-center justify-center max-sm:hidden relative bottom-1">
+            <Link href={'/'} id="link" className="mx-3 font-semibold p-2 dark:hover:bg-white/10 rounded-md transition-colors ">Overview</Link>
+            <Link id="link" href={'#projects'} className="mx-3 font-semibold p-2 dark:hover:bg-white/10 rounded-md transition-colors">Projects</Link>
+            <Link id="link" href={'#experience'} className="mx-3 font-semibold p-2 dark:hover:bg-white/10 rounded-md transition-colors ">Experience</Link>
+            <Link id="link" href={'#contact'} className="mx-3 font-semibold p-2  dark:hover:bg-white/10 rounded-md transition-colors">Contact</Link>
+            <span id="separator" className="border-l-green text-white/10">|</span>
+            <ThemeToggle />
+        </div>
+    )
+}
+import { Menu, ExternalLink } from 'lucide-react';
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function DropMenu() {
+    return (
+        <div className="min-sm:hidden bg-black/10 rounded-md">
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline"><Menu /></Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-30 bg-black/10 backdrop-blur-md" align="start">
+                    <DropdownMenuLabel>Overview</DropdownMenuLabel>
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                            Skills
+                            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Projects
+                            {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Contact
+                            {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
+                        </DropdownMenuItem>
+
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>GitHub
+                        <DropdownMenuShortcut><ExternalLink width={15} height={15} /></DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>Linkedin
+                        <DropdownMenuShortcut><ExternalLink width={15} height={15} /></DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    {/* <DropdownMenuSeparator /> */}
+                    <ThemeToggle />
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    )
+}
+
+
---
+++ src/components/Overview.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import Header from "./Header";
+import { DropdownMenuSeparator } from "./ui/dropdown-menu";
+export default function Overview() {
+    return (
+        <>
+            <Header />
+            <div className="flex mx- max-sm:mx-3 justify-center">
+                <div className="bg-fiber-carbon flex h-screen flex-col items-  mt-20">
+                    <div className="max-w-sm rounded-md bg-whte/10 p-5 backdrop-blur-md text-center items-center" id="">
+                        <div className="justify-items-center">
+                            <h1 className="text-3xl font-bold p-4"> Hi, I am <i>Arslan.M</i> </h1>
+                            <Image
+                                src="/assets/profile.png"
+                                alt="profile"
+                                width={200}
+                                height={200}
+                            />
+                            <h2 className="text-2xl font-bold mt-2 p-4">The Full Stack Developer</h2>
+                        </div>
+                        <DropdownMenuSeparator />
+                        <div className="justify-items-center">
+                            <h3 className="text-xl font-semibold">Expertises in</h3>
+                            <div className="flex max-sm:flex-wrap max-sm:justify-center">
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/laravel.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                    <h4>Laravel</h4>
+                                </div>
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/react.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                    <h4>React</h4>
+                                </div>
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/next.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                        className="invert brightness-0"
+                                        id="imginvert"
+                                    />
+                                    <h4>Next.js</h4>
+                                </div>
+                                <div className="p-2 justify-items-center mt-3">
+                                    <Image
+                                        src="/assets/sql.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                        className="invert brightness-0"
+                                        id="imginvert"
+                                    />
+                                    <h4>SQL</h4>
+                                </div>
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/tailwind.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                        
+                                    />
+                                    <h4>Tailwind</h4>
+                                </div>
+
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/bootstrap.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                    <h4>Bootstrap</h4>
+                                </div>
+                                <div className="p-2 justify-items-center">
+                                    <Image
+                                        src="/assets/wordpress.webp"
+                                        alt="logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                    <h4>Wordpress</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
