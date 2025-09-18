cf0ef0ae4bb9408a420e02c6f699f8fc7b5493e2
arslan433 <arslan.reactdev@gmail.com>
2025-09-18 16:59:13 +0500

    Projects page added

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -16,27 +16,31 @@ export default function Projects() {
                 </div>
                 {/* Projects listing  */}
                 <div className="justify-items-center px-2 mt-15 items-center">
-                    <div className="border-1 h-[190px] max-sm:h-auto w-3/5 max-sm:w-full mb-2 pb-3" >
-                        <div className="float-left p-4">
-                            <h4 className="font-semibold">Project Name Heading</h4>
+                    <div className="border-1 h-48 w-3/5 max-sm:w-full mb-2 pb-3 px-4" >
+                        <div className="py-4 text-left">
+                            <h4 className="font-semibold text-xl">Project Name Heading</h4>
                         </div>
                         <div>
-                            <p className="float-left text-left px-4 mb-2">These are most of the projects I've done since I started programming, some of them are personstarted programming, some of them are personstarted programming, some of them are personal projects, freelance, work,</p>
+                            <p className="text-left mb-2">These are most of the projects I've done since I started programming, some of them are personstarted programming, some of them are personstarted programming, some of them are personal projects, freelance, work,</p>
 
                         </div>
-                        <div className="float-right flex gap-4 px-2 m-2">
-                            <p>
-                                <Image
-                                    src="/assets/github.webp"
-                                    alt="logo"
-                                    width={30}
-                                    height={30}
-                                />
-                            </p>
-                            <p><Eye height={30} width={30}/></p>
+                        <div className="flex justify-between items-start flex-wrap max-md:flex-column flex-row gap-5 mt-2">
+                            <div>
+                                <p className="font-[700] tracking-tight">Javascript React</p>
+                            </div>
+                            <div className="flex justify-center items-end gap-3">
+                                <p>
+                                    <Image
+                                        src="/assets/github.webp"
+                                        alt="logo"
+                                        width={30}
+                                        height={30}
+                                    />
+                                </p>
+                                <p><Eye height={30} width={30} /></p>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
